e6e77c340642b082cb13998ca6c415793dae064e
Umesh Bagade <71688971+umeshbagade@users.noreply.github.com>
2022-01-10 11:55:18 +0530

    Create life_universe_and_everything.cpp

---
+++ life_universe_and_everything.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	while(1)
+	{
+	    cin>>n;
+	    if(n==42)
+	    {
+	        break;
+	    }
+	    cout<<n<<endl;
+	}
+	return 0;
+}
