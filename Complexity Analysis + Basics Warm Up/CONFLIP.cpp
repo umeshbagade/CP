329eee2bd22e533d3226d462cea5894ec7aa86ce
Umesh Bagade <71688971+umeshbagade@users.noreply.github.com>
2022-01-15 19:03:29 +0530

    Create CONFLIP.cpp

---
+++ Complexity Analysis + Basics Warm Up/CONFLIP.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int t,j,g,i,n,q;
+  cin>>t;
+  while(t>0)
+  {
+    cin>>g;
+    for(j=0;j<g;j++)
+    {
+      cin>>i>>n>>q;
+      //print value
+      if(n%2==0)
+      {
+        cout<<n/2<<endl;
+      }
+      else
+      {
+        if(i==1)
+        {
+            //process
+          if(q==1)
+          {
+            cout<<n/2<<endl;
+          }
+          else
+          {
+            cout<<(n+1)/2<<endl;
+          }
+        }
+        else if(i==2)
+        {
+            //process
+          if(q==1)
+          {
+            cout<<(n+1)/2<<endl;
+          }
+          else
+          {
+            cout<<n/2<<endl;
+          }
+        }
+      }
+    }
+    t--;
+  }
+}
